fa88aa1657be4e5d785cb58bd651b358af05642e
Yukta Vajpayee <65439646+Yukta30@users.noreply.github.com>
2022-02-17 23:50:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -134,7 +134,11 @@ app.post("/delete", function(req, res){
 app.get("/about", function(req, res){
   res.render("about");
 });
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port=3000;
+}
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(port, function(){
+  console.log("Server started on port 3000");
 });
